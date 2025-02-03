bc35c9f57de6814e04c91bc308a415a62dcdef54
yura.cheglakov <yura.cheglakov@gmail.com>
2025-02-03 23:17:39 +0300

    ДЗ 15.1: фикстуры, параметризация

--- homework/test_parametrize.py
+++ homework/test_parametrize.py
@@ -2,16 +2,19 @@
 Переопределите параметр с помощью indirect параметризации на уровне теста
 """
 import pytest
+from selene import browser as window
 
+@pytest.fixture(params=["mobile", "desktop"])
+def browser(request):
+    if request.param == "mobile":
+        return window.driver.set_window_size(640, 960)
+    if request.param == "desktop":
+        return window.driver.set_window_size(1920, 1080)
 
-@pytest.fixture()
-def browser():
-    pass
-
-
+@pytest.mark.parametrize("browser", ["desktop"], indirect=True)
 def test_github_desktop(browser):
-    pass
+    window.open('https://github.com/')
 
-
+@pytest.mark.parametrize("browser", ["mobile"], indirect=True)
 def test_github_mobile(browser):
-    pass
+    window.open('https://github.com/')
--- homework/test_skip.py
+++ homework/test_skip.py
@@ -3,16 +3,19 @@
 Пропустите мобильный тест, если соотношение сторон десктопное (и наоборот)
 """
 import pytest
+from selene import browser as window
 
+@pytest.fixture(params=["mobile", "desktop"])
+def browser(request):
+    if request.param == "mobile":
+        window.driver.set_window_size(640, 960)
+    if request.param == "desktop":
+        window.driver.set_window_size(1920, 1080)
 
-@pytest.fixture()
-def browser():
-    pass
-
-
+@pytest.mark.parametrize("browser", ["desktop"], indirect=True)
 def test_github_desktop(browser):
     pass
 
-
+@pytest.mark.parametrize("browser", ["mobile"], indirect=True)
 def test_github_mobile(browser):
     pass
